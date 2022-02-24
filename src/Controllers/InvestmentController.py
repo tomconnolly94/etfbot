4ff6245d667ebcb0c493289d8a3dec70a61fc630
Tom <tom.connolly@protonmail.com>
2022-02-24 23:12:52 +0100

    improved commenting

--- src/Controllers/InvestmentController.py
+++ src/Controllers/InvestmentController.py
@@ -8,9 +8,17 @@ from Interfaces.AlpacaInterface import AlpacaInterface
 from Interfaces.SP500IndexInterface import SP500IndexInterface
 from Types.StockData import StockData
 
+"""
+InvestmentController
 
+This is a class to control investment decisions
+
+"""
 class InvestmentController:
 
+    """
+    `__init__`: intialise object fields
+    """
     def __init__(self: object):
         # class fields
         self.idealStockRangeIndexStart = 300
@@ -21,6 +29,9 @@ class InvestmentController:
         self.sp500IndexInterface = SP500IndexInterface()
 
 
+    """
+    `_getIdealPositions`: get a list (symbols and quantities) of the ideal stocks to invest in based on the portfolio value
+    """
     def _getIdealPositions(self: object) -> 'dict[str, int]':
         idealStockRange = self._getStockRangeIdeal()
         portfolioValue = self.alpacaInterface.getPortfolioValue()
@@ -29,25 +40,38 @@ class InvestmentController:
         return idealPositions
 
 
+    """
+    `_getStockRangeIdeal`: get a limited list (StockData) of the ideal stocks to invest in
+    """
     def _getStockRangeIdeal(self: object) -> 'list[StockData]':
         if not self._sortedFullStockCache:
             self._refreshStockCache()
         return self._sortedFullStockCache[self.idealStockRangeIndexStart:self.idealStockRangeIndexEnd]
 
 
+    """
+    `_refreshStockCache`: save list of StockData items, prices and symbols from index
+    """
     def _refreshStockCache(self: object) -> None:
         stockSymbols = self.sp500IndexInterface.getIndexSymbols()
         stockDataList = self.alpacaInterface.getStockDataList(stockSymbols)
         self._sortedFullStockCache = sorted(stockDataList, key=lambda x: x.price, reverse=True)
 
 
+    """
+    `_getStockRangeDontSell`: get a list of StockData items that fall in or above the "ideal" stock range
+    """
     def _getStockRangeDontSell(self: object) -> 'list[StockData]':
         if not self._sortedFullStockCache:
             self._refreshStockCache()
         return self._sortedFullStockCache[:self.idealStockRangeIndexEnd]
 
 
-    # temp debug funcs
+    ########## debug funcs ##########
+
+    """
+    `_getPositionInIndex`: get position of a stock in the index by symbol
+    """
     def _getPositionInIndex(self: object, stockDataSymbol: str):
         for index, cachedStock in enumerate(self._sortedFullStockCache):
             if cachedStock.symbol == stockDataSymbol:
@@ -55,11 +79,19 @@ class InvestmentController:
         return None
 
 
+    """
+    `_printPositions`: pretty print symbols and index positions
+    """
     def _printPositions(self: object, positions: 'dict[str, int]'):
         for positionSymbol in positions.keys():
             print(f"{positionSymbol}: {self._getPositionInIndex(positionSymbol)}")
 
-    
+    ########## debug funcs ##########
+
+
+    """
+    `_getPositionsToSell`: get dict of symbols and quantities of open positions that should be closed
+    """
     def _getPositionsToSell(self: object, currentPositions: 'dict[str, int]') -> 'dict[str, int]':
         dontSellStockRange = [ stockData.symbol for stockData in self._getStockRangeDontSell() ]
         positions = dict(filter(lambda entry: entry[0] not in dontSellStockRange, currentPositions.items()))
@@ -70,26 +102,33 @@ class InvestmentController:
         return positions
 
 
-    def _getPositionsToBuy(self: object, currentPositions: 'dict[str, int]', valueOfPositionsToSell: float) -> 'dict[str, int]':
-        idealPositionsToBuy = dict(filter(lambda entry: entry.symbol not in currentPositions, self._getStockRangeIdeal()))
+    """
+    `_getPositionsToBuy`: get dict of symbols and quantities of open positions that should be opened with the available funds
+    """
+    def _getPositionsToBuy(self: object, currentPositions: 'dict[str, int]', availableFunds: float) -> 'dict[str, int]':
+        idealPositionsToBuy = list(filter(lambda entry: entry.symbol not in currentPositions, self._getStockRangeIdeal()))
         runningPositionsTotalValue = 0
         actualPositionsToBuy = {}
 
         for stockData in idealPositionsToBuy:
             runningPositionsTotalValue += stockData.price
-            if runningPositionsTotalValue <= valueOfPositionsToSell:
+            if runningPositionsTotalValue <= availableFunds:
                 actualPositionsToBuy[stockData.symbol] = 1
-            else:
-                break
 
         return actualPositionsToBuy
 
 
+    """
+    `_getValueOfStockList`: take a list of positions, find their value and return the sum
+    """
     def _getValueOfStockList(self: object, stockSymbolList: 'dict[str, int]') -> float:
         relevantStockDataList: 'list[StockData]' = [ stockData for stockData in self._sortedFullStockCache if stockData.symbol in stockSymbolList.keys() ]
         return sum(stockData.price for stockData in relevantStockDataList)
 
 
+    """
+    `rebalanceInvestments`: analyse open positions to look for positions to close, and replace any closed positions with new openings
+    """
     def rebalanceInvestments(self):
 
         # get current positions
@@ -98,9 +137,12 @@ class InvestmentController:
         # calculate trades that should be made to turn current position into ideal position
         positionsToSell = self._getPositionsToSell(currentPositions)
         valueOfPositionsToSell: float = self._getValueOfStockList(positionsToSell)
+        availableFunds = self.alpacaInterface.getAvailableFunds()
+        totalBuyingPower = valueOfPositionsToSell + availableFunds
         positionsToBuy = {}
-        if valueOfPositionsToSell > 0:
-            positionsToBuy = self._getPositionsToBuy(currentPositions, valueOfPositionsToSell)
+
+        if totalBuyingPower > 0:
+            positionsToBuy = self._getPositionsToBuy(currentPositions, totalBuyingPower)
 
         # make trades
         for positionKey, positionQuantity in positionsToSell.items():
