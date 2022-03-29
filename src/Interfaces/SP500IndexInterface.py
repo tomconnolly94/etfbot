1ad9bcddc45f68362e077c401c3901697f2560ee
tconnolly-nfx <85183965+tconnolly-nfx@users.noreply.github.com>
2022-03-29 23:18:33 +0200

    first steps toward configurability

--- src/Interfaces/SP500IndexInterface.py
+++ src/Interfaces/SP500IndexInterface.py
@@ -1,27 +1,51 @@
 #!/usr/bin/python
 
 # external dependencies
+from enum import Enum
 import finsymbols
 
 
+class StockExchange(Enum):
+    NASDAQ = 1
+    SP500 = 2
+    AMEX = 3
+    NYSE = 4
+    FTSE100 = 5
+
+
 """
-SP500IndexInterface
+StockIndexDataInterface
 
 This is a class to encapsulate all interactions between the software and the
 finsymbols library
 
 """
-class SP500IndexInterface:
+class StockIndexDataInterface:
 
     def __init__(self: object):
+        self._stockExchangesUSA = [ StockExchange.NASDAQ, StockExchange.SP500, StockExchange.AMEX, StockExchange.NYSE ]
+        self._stockExchangesUK = [ StockExchange.FTSE100 ]
         pass
 
 
     """
     `getIndexSymbols`: returns a list of symbols in the S&P500 index 
     """
-    def getIndexSymbols(self: object) -> 'list[str]':
-        sp500 = finsymbols.get_sp500_symbols()
-        symbols = [item["symbol"].replace("\n", "") for item in sp500]
-        return symbols
+    def getIndexSymbols(self: object, stockExchange: StockExchange) -> 'list[str]':
+        if stockExchange == StockExchange.NASDAQ:
+            return self.__processFinSymbolsResults(finsymbols.get_nasdaq_symbols())
+        if stockExchange == StockExchange.SP500:
+            return self.__processFinSymbolsResults(finsymbols.get_sp500_symbols())
+        if stockExchange == StockExchange.AMEX:
+            return self.__processFinSymbolsResults(finsymbols.get_amex_symbols())
+        if stockExchange == StockExchange.NYSE:
+            return self.__processFinSymbolsResults(finsymbols.get_nyse_symbols())
+        if stockExchange == StockExchange.FTSE100:
+            raise NotImplementedError("FTSE 100 Stock Exchange data not connected yet")
         
+    
+    """
+    `__processFinSymbolsResults`: processes the raw data from finsymbols into a list of stock symbols 
+    """
+    def __processFinSymbolsResults(self, input):
+        return [item["symbol"].replace("\n", "") for item in input]
