996f4ca064c7ba8cb8103f708e154bd8c6c85f6d
Tom <tom@emal.com>
2023-03-22 22:31:08 +0000

    updated logging controller

--- src/Controllers/LoggingController.py
+++ src/Controllers/LoggingController.py
@@ -13,11 +13,12 @@ def initLogging():
     :return: the configured logging object
     """
     logFormat = '%(asctime)s - %(levelname)s - %(message)s'
+    programName = "etfbot"
     logDateFormat = '%d-%b-%y %H:%M:%S'
     projectBaseDir = os.getenv("LOGS_DIR") if os.getenv("LOGS_DIR") else os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "logs")
 
     if os.getenv("ENVIRONMENT") == "production":
-        logFilename = os.path.join(projectBaseDir, f"media-grab_{time.strftime('%d-%m-%Y_%H-%M')}.log")
+        logFilename = os.path.join(projectBaseDir, f"{programName}_{time.strftime('%d-%m-%Y_%H-%M')}.log")
         logging.basicConfig(filename=logFilename, filemode='w', format=logFormat, datefmt=logDateFormat, level=logging.INFO)
     else:
         logging.basicConfig(format=logFormat, datefmt=logDateFormat, level=logging.INFO)
