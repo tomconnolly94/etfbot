d7045965f00351a4a72a130e950aaf24163c9b30
Tom <tom@emal.com>
2024-03-07 23:41:06 +0000

    sync'd requirements between app parts, added a few unit tests

--- webapp/server/test/controllers/test_DataServer.py
+++ webapp/server/test/controllers/test_DataServer.py
@@ -2,110 +2,106 @@
 
 # external dependencies
 import json
+from unittest.mock import MagicMock
 from server.test.testUtilities import FakeFile
 import unittest
 import mock
 
 # internal dependencies
-from server.controllers.DataServer import deleteMediaInfoRecord, runMediaGrab, serveMediaInfo, submitMediaInfoRecord, updateMediaInfoRecord
+from server.controllers.DataServer import _normaliseValues, _getSPY500Data, _getCurrentHoldingsPerformanceData
 
 
 class Test_DataServer(unittest.TestCase):
 
-    @mock.patch("os.getenv")
-    @mock.patch("builtins.open", create=True)
-    def test_serveMediaInfo(self, openMock, osGetEnvMock):
-
-        #config fake data
-        fakeFileContent = "fakeFileContent"
-
-        # config mocks
-        osGetEnvMock.return_value = "fake env return"
-        openMock.return_value = FakeFile(fakeFileContent)
-
-        content = serveMediaInfo()
-
-        self.assertEqual(fakeFileContent, content)
-        
-
-    @mock.patch("server.controllers.DataServer.writeNewRecordToMediaInfoFile")
-    def test_submitMediaInfoRecord(self, writeNewRecordToMediaInfoFileMock):
+    def test__normaliseValues(self):
 
         #config fake data
         fakeData = {
-            "mediaName": "fakeMediaName",
-            "latestSeason": "1",
-            "latestEpisode": "2",
-            "blacklistTerms": "blacklistTerm1,blacklistTerm2 , blacklistTerm3  "
+            "key1": 1,
+            "key2": 2,
+            "key3": 3,
+            "key4": 4,
+            "key5": 5
+        }
+        expectedNormalisedValues = {
+            "key1": 0.2,
+            "key2": 0.4,
+            "key3": 0.6,
+            "key4": 0.8,
+            "key5": 1
         }
 
-        expectedMediaName = "fakeMediaName"
-        expectedLatestSeason = 1
-        expectedLatestEpisode = 2
-        expectedBlacklistTerms = ["blacklistTerm1", "blacklistTerm2", "blacklistTerm3" ]
+        normalisedValues = _normaliseValues(fakeData)
 
-        # config mock
-        writeNewRecordToMediaInfoFileMock.return_value = True
-        
-        result = submitMediaInfoRecord(fakeData)
-
-        self.assertEqual(True, result)
-        writeNewRecordToMediaInfoFileMock.assert_called_with(expectedMediaName, expectedLatestSeason, expectedLatestEpisode, expectedBlacklistTerms)
+        self.assertEqual(expectedNormalisedValues, normalisedValues)
 
 
-    @mock.patch("server.controllers.DataServer.removeRecordFromMediaInfoFile")
-    def test_deleteMediaInfoRecord(self, removeRecordFromMediaInfoFileMock):
+    @mock.patch("server.controllers.DataServer._normaliseValues")
+    @mock.patch("server.controllers.DataServer.getPricesForStockSymbols")
+    def test__getSPY500Data(self, getPricesForStockSymbolsMock, _normaliseValuesMock):
 
         # config fake data
-        recordIndex = "2"
-
-        # config mock
-        removeRecordFromMediaInfoFileMock.return_value = True
-        
-        result = deleteMediaInfoRecord(recordIndex)
-
-        self.assertEqual(True, result)
-        removeRecordFromMediaInfoFileMock.assert_called_with(recordIndex)
-
-
-    @mock.patch("server.controllers.DataServer.updateRecordInMediaInfoFile")
-    def test_updateMediaInfoRecord(self, updateRecordInMediaInfoFileMock):
-
-        # config fake data
-        fakeNewMediaIndexRecord = {
-            "name": "fakeMediaIndexRecordName",
-            "latestEpisode": 2,
-            "latestSeason": 3,
-            "blacklistTerms": ["blacklistTerm1", "blacklistTerm1"]
+        fakeStockData = {
+            "stock1": 10,
+            "stock2": 15,
+            "stock3": 20,
+            "stock4": 50,
+            "stock5": 100,
         }
-        fakeStringifiedNewMediaIndexRecord = json.dumps(fakeNewMediaIndexRecord)
-        fakeRecordIndex = 2
+        expectedStockData = {
+            "startValue": 10,
+            "endValue": 100,
+            "values": fakeStockData
+        }
 
-        # config mock
-        updateRecordInMediaInfoFileMock.return_value = True
-        
-        result = updateMediaInfoRecord(fakeStringifiedNewMediaIndexRecord, fakeRecordIndex)
-
-        self.assertEqual(True, result)
-        updateRecordInMediaInfoFileMock.assert_called_with(fakeNewMediaIndexRecord, fakeRecordIndex)
+        # config mocks
+        getPricesForStockSymbolsMock.return_value = [fakeStockData]
+        _normaliseValuesMock.return_value = fakeStockData
 
 
-    @mock.patch("subprocess.check_call")
-    @mock.patch("os.getenv")
-    def test_runMediaGrab(self, osGetEnvMock, checkCallMock):
+        stockData = _getSPY500Data()
+
+        self.assertEqual(expectedStockData, stockData)
+    
+    
+    @mock.patch("server.controllers.DataServer._normaliseValues")
+    @mock.patch("server.controllers.DataServer.getPricesForStockSymbols")
+    @mock.patch("server.controllers.DataServer.AlpacaInterface")
+    def test__getCurrentHoldingsPerformanceData(self, AlpacaInterfaceMock, getPricesForStockSymbolsMock, _normaliseValuesMock):
 
         # config fake data
-        fakeOsGetEnvValue = "fakeOsGetEnvValue"
+        openPositions = {
+            "stock1": 10,
+            "stock2": 15,
+            "stock3": 20,
+            "stock4": 50,
+            "stock5": 100,
+        }
+        historicalPositionPrices = [
+            { "date1": 2, "date2": 7, "date3": 3, "date4": 10 },
+            { "date1": 12, "date2": 15, "date3": 6, "date4": 15 },
+            { "date1": 24, "date2": 14, "date3": 17, "date4": 20 },
+            { "date1": 21, "date2": 18, "date3": 58, "date4": 50 },
+            { "date1": 76, "date2": 1, "date3": 38, "date4": 100 },
+        ]
+        expectedReturnValue = {
+            'startValue': 135, 
+            'endValue': 195, 
+            'values': {'date1': 135, 'date2': 55, 'date3': 122, 'date4': 195}}
 
-        # config mock
-        osGetEnvMock.return_value = "fakeOsGetEnvValue"
+        # config mocks
+        AlpacaInterfaceMagicMock = MagicMock()
+        AlpacaInterfaceMagicMock.getOpenPositions.return_value = openPositions
+        AlpacaInterfaceMock.return_value = AlpacaInterfaceMagicMock
+        getPricesForStockSymbolsMock.return_value = historicalPositionPrices
+        _normaliseValuesMock.side_effect = lambda input: input
 
-        result = runMediaGrab()
+        stockData = _getCurrentHoldingsPerformanceData()
 
+        expectedEndValue = sum([ price["date1"] for price in historicalPositionPrices ])
 
-        checkCallMock.assert_called_with(['venv/bin/python', 'Main.py'], cwd=fakeOsGetEnvValue)
-
-
+        self.assertEqual(stockData["endValue"], sum(openPositions.values()))
+        self.assertEqual(stockData["startValue"], expectedEndValue)
 
 
 if __name__ == "__main__":
