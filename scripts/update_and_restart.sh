863c262a6bfebec1baefac3a957e3c3bac1fbd47
Tom <tom@emal.com>
2024-03-08 19:43:43 +0000

    added db volume to docker run command

--- scripts/update_and_restart.sh
+++ scripts/update_and_restart.sh
@@ -15,8 +15,8 @@ docker container prune -f
 # move to project root
 cd $SCRIPTDIR/..
 
-# rebuild and restart containers in daemon mode
+# rebuild and restart containers in daemon mode, relies on volume "etfbot-db-volume" existing
 docker build --no-cache -t etfbot .
-docker run -p 8080:8080 --name etfbot -d etfbot:latest
+docker run --mount source=etfbot-db-volume,target=/db -p 8080:8080 --name etfbot -d etfbot:latest
 
 cd -
