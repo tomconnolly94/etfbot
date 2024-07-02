d938ccc35b58cff563ef94bbf49d8df9c15e3753
Tom <tom@emal.com>
2024-07-02 20:49:12 +0100

    improvements

--- scripts/update_and_restart.sh
+++ scripts/update_and_restart.sh
@@ -19,11 +19,10 @@ cd $SCRIPTDIR/..
 # docker build --no-cache -t etfbot .
 docker build -t etfbot .
 
-# exit if docker builds fails
+# exit if docker build fails
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
-
 
 docker run --mount source=etfbot-db-volume,target=/db -p 8080:8080 --name etfbot -d etfbot:latest
 
@@ -31,6 +30,5 @@ docker run --mount source=etfbot-db-volume,target=/db -p 8080:8080 --name etfbot
 if [[ $? -ne 0 ]] ; then
     exit 2
 fi
-
 
 cd -
