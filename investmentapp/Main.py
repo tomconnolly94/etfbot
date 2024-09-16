a94f8a0cd41a21e3e55f02b27c0617cf8f7891d6
Tom <tom@emal.com>
2024-09-16 20:42:42 +0100

    reorganised start/end log

--- investmentapp/Main.py
+++ investmentapp/Main.py
@@ -16,25 +16,28 @@ from investmentapp.src.Interfaces.MailInterface import MailInterface
 
 def main():
 
+    logging.info(f"Program started.")
+
     # load environment
     load_dotenv()
 
     # intitialise logging module
     LoggingController.initLogging()
 
-    logging.info(f"Program started.")
-
     errorString = ""
 
     # run program with general error handling to prevent crashes
     try:
         InvestmentController().rebalanceInvestments()
+        logging.info(f"Program executed successfully.")
     except Exception as exception:
         logging.error(exception)
         errorString = exception
+        logging.info(f"Program executed with errors.")
 
     # send email notification with the log and any unhandled exceptions
     MailInterface().sendInvestmentAppSummaryMail(success=False if errorString else True)
+    logging.info(f"Program ended.")
 
 
 if __name__ == "__main__":
