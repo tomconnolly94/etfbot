184c0d1ac3157ef92e46fc7552865158a1fb35a7
Tom <tom@emal.com>
2024-09-28 15:13:22 +0100

    add traceback output to generic exception handler for more info about undhandled exceptions

--- investmentapp/Main.py
+++ investmentapp/Main.py
@@ -4,13 +4,16 @@
 from dotenv import load_dotenv
 import logging
 import sys, os
+import traceback
 
 # internal dependencies
 sys.path.append(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 )  # make webapp and common sub projects accessible
 from common import LoggingController
-from investmentapp.src.Controllers.InvestmentController import InvestmentController
+from investmentapp.src.Controllers.InvestmentController import (
+    InvestmentController,
+)
 from investmentapp.src.Interfaces.MailInterface import MailInterface
 
 
@@ -31,12 +34,17 @@ def main():
         InvestmentController().rebalanceInvestments()
         logging.info(f"Program executed successfully.")
     except Exception as exception:
-        logging.error(exception)
-        errorString = exception
-        logging.info(f"Program executed with errors.")
+        errorString = (
+            "Exception occured during program run ("
+            "caught by generic exception handler):"
+            f"\n{traceback.format_exc()}".rstrip()
+        )
+        logging.error(errorString)
 
     # send email notification with the log and any unhandled exceptions
-    MailInterface().sendInvestmentAppSummaryMail(success=False if errorString else True)
+    MailInterface().sendInvestmentAppSummaryMail(
+        success=False if errorString else True
+    )
     logging.info(f"Program ended.")
 
 
